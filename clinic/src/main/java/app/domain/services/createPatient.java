aa762165fca00de4769cb2567b6807b6e65e7ae4
andfsanchezag <andres.sanchez57@tdea.edu.co>
2026-02-19 16:37:54 -0500

    add services

--- clinic/src/main/java/app/domain/services/createPatient.java
+++ clinic/src/main/java/app/domain/services/createPatient.java
@@ -1,18 +1,17 @@
 package app.domain.services;
 
-import app.domain.models.EmergencyContact;
 import app.domain.models.Patient;
 import app.domain.ports.ContactEmergencyPort;
 import app.domain.ports.PatientPort;
 import app.domain.Exceptions.BusinessException;
 
-public class createPatient {
+public class CreatePatient {
 
     private PatientPort patientPort;
     private ContactEmergencyPort emergencyContactPort;
     private CreateEmergencyContact createEmergencyContact;
 
-    public void createPatient(app.domain.models.Patient patient) {
+    public void createPatient(Patient patient) throws BusinessException {
         // Validar que el paciente no exista en la base de datos
         // Guardar el paciente en la base de datos
         if (patientPort.existsByDocument(patient.getDocument())) {
